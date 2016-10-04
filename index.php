a5d7a1a31986e459753c84fcfd1966e9977e7307
Tejas Shah <tejashah88@gmail.com>
2016-10-03 20:14:25 -0700

    changed to .php + adjusted intro page

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 		<meta name="description" content="">
-		<meta name="author" content="">
+		<meta name="author" content="Tejas Shah">
 		<!--<link rel="icon" href="../../favicon.ico">-->
-		<title>Login | ARTIK Home</title>
+		<title>Login/Signup | SM Popular Stats</title>
 		<!-- Bootstrap core CSS -->
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
@@ -30,8 +30,51 @@
 			<div class="container">
 				<div class="starter-template">
 					<h1>Social Media Statistics</h1>
-					<p class="lead">Welcome to the official social media statistics website! Please login to link your account or check you device statuses.</p>
-					<button type="button" class="btn btn-lg btn-primary" onClick="location.href=">Login</button>
+					<p class="lead">A website that lists various statistics of your profile of popular social media.</p>
+				</div>
+
+				<div class="row">
+					<div class="col-xs-6">
+						<h2>Login</h2>
+
+						<form id="loginForm">
+							<div class="form-group">
+								<label for="txtLoginUsername">Username</label>
+								<input type="text" class="form-control" id="txtLoginUsername" placeholder="Enter username">
+							</div>
+
+							<div class="form-group">
+								<label for="txtLoginPassword">Password</label>
+								<input type="password" class="form-control" id="txtLoginPassword" placeholder="Password">
+							</div>
+
+							<button id="btnSubmitLogin" type="submit" class="btn btn-primary">Submit</button>
+						</form>
+					</div>
+
+					<div class="col-xs-6">
+						<h2>Sign-Up</h2>
+
+						<form id="loginForm">
+							<div class="form-group">
+								<label for="txtSignupEmail">Email address</label>
+								<input type="email" class="form-control" id="txtSignupEmail" aria-describedby="emailHelp" placeholder="Enter email">
+								<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+							</div>
+
+							<div class="form-group">
+								<label for="txtSignupUsername">Username</label>
+								<input type="text" class="form-control" id="txtSignupUsername" placeholder="Enter username">
+							</div>
+
+							<div class="form-group">
+								<label for="txtSignupPassword">Password</label>
+								<input type="password" class="form-control" id="txtSignupPassword" placeholder="Password">
+							</div>
+
+							<button id="btnSubmitSignup" type="submit" class="btn btn-primary">Submit</button>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -48,14 +91,16 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
 		<script src="js/bootstrap.min.js"></script>
-		<script>
-			query = window.location.hash.split("#");
-			if (query[1]) { 
-				// accounts sends an URL fragment in the login callback,
-				// Now make web browser to load a new url
-				window.location = "main.php?"+query[1];
-			}
+		<script type="text/javascript">
+			$('#btnSubmitLogin').on('click', (e) => {
+				alert("Login");
+			})
+
+			$('#btnSubmitSignup').on('click', (e) => {
+				alert("Signup");
+			})
 		</script>
+
 		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 		<script src="assets/js/ie10-viewport-bug-workaround.js"></script>
 	</body>
